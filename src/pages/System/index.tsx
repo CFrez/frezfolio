ebaffa41fdcd8de7fe09d3b5ce75f914713c943f
RedFrez <30352852+RedFrez@users.noreply.github.com>
2024-02-25 15:21:32 -0600

    fix responsive

--- src/pages/System/index.tsx
+++ src/pages/System/index.tsx
@@ -10,16 +10,7 @@ import './index.sass'
 export const System: React.FC = () => {
     return (
         <main id="system">
-            <p className='info'>
-                Some people have names and then I started getting lazy and so
-                others are only letters. As a system, we go by Christina. Some
-                of our discovery was sabotaged by one member making up people...
-                so we kinda took a break and reset and these are the ones that
-                have been more &quot;formalized&quot; since. This is designed
-                to be a living document that will grow and change as we discover
-                more about our system.
-            </p>
-            <Tabs unstyled allowTabDeactivation>
+            <Tabs unstyled defaultValue='system'>
                 <Tabs.List>
                     {alters.map((alter: Alter) => (
                         <Tabs.Tab key={alter.name} value={alter.name}>
@@ -28,6 +19,17 @@ export const System: React.FC = () => {
                         </Tabs.Tab>
                     ))}
                 </Tabs.List>
+                <Tabs.Panel value='system'>
+                    <p className='info'>
+                        Some people have names and then I started getting lazy and so
+                        others are only letters. As a system, we go by Christina. Some
+                        of our discovery was sabotaged by one member making up people...
+                        so we kinda took a break and reset and these are the ones that
+                        have been more &quot;formalized&quot; since. This is designed
+                        to be a living document that will grow and change as we discover
+                        more about our system.
+                    </p>
+                </Tabs.Panel>
                 {alters.map((alter: Alter) => (
                     <Tabs.Panel key={alter.name} value={alter.name}>
                         {alter.avatar}
