3957eef3b0a075d1db9f5a49fa7f2d8b575d3463
Christina Frezynski <christina@frez.me>
2024-11-26 15:27:37 -0600

    add missing slash

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -12,18 +12,12 @@ export const Header: React.FC = () => {
         <header
             className={`
             bg-contain bg-repeat-x bg-center
-            bg-[url(images/topper/light-sky.svg)] 
-            dark:bg-[url(images/topper/dark-sky.svg)]
+            bg-[url(/images/topper/light-sky.svg)] 
+            dark:bg-[url(/images/topper/dark-sky.svg)]
             border-b-2 sm:border-b-[6px] 
             border-neutral-800
         `}
         >
-            {/* 
-                hack to try and force github pages to publish what it doesn't
-                seem to notice in the classname...
-             */}
-            <img src="images/topper/dark-sky.svg" className="hidden" />
-            <img src="images/topper/light-sky.svg" className="hidden" />
             <div className="flex flex-col w-full gap-0 items-center justify-center">
                 <div className="flex flex-row w-full max-w-7xl justify-between items-start px-6 pt-4">
                     <div className="flex flex-row gap-6">
